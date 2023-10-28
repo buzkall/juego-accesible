990f5f2c73675e9a9f06a029a9aa47c630cf557e
Alicia Rodríguez <alicia@arzcode.com>
2023-10-28 12:08:42 +0200

    First version of the game

---
+++ app/Livewire/Game.php
@@ -0,0 +1,269 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Game extends Component
+{
+    public $board = [];
+    public $time = 30; // seconds
+    public $score = 0;
+    public $alive = true;
+    public $hasWon = false;
+    public $x = 0;
+    public $y = 1;
+    public $announcements = [];
+    public $showInfo = false;
+    public $showBoard = false;
+    public $movement = null;
+
+    public function mount()
+    {
+        $this->showInfo = true;
+        //$this->showInfo = false;
+        //$this->showBoard = true; // TBR
+        $this->generateBoard();
+    }
+
+    public function tick()
+    {
+        if ($this->alive && ! $this->hasWon) {
+            $this->time--;
+
+            if (isset($this->board[$this->y][$this->x]) && $this->board[$this->y][$this->x]['gravity'] == 0) {
+                $this->board[$this->y][$this->x]['element'] = '';
+                $this->y++;
+                $this->announcements = [];
+                $this->checkNewPosition();
+            }
+
+            if ($this->time === 0) {
+                $this->alive = false;
+                $this->announcements = ['Game Over'];
+            }
+        }
+    }
+
+    public function startGame()
+    {
+        $this->showInfo = false;
+        $this->showBoard = true;
+
+        $this->ask();
+    }
+
+    public function generateBoard()
+    {
+        $this->board = [
+            [
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 0, 'win' => 0, 'element' => '🪙', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 0, 'win' => 0, 'element' => '🪙', 'color' => 'bg-sky-200'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+            ],
+            [
+                ['gravity' => 1, 'win' => 0, 'element' => '💃', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 0, 'element' => '🪨', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 0, 'element' => '🪙', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 0, 'element' => '🪨', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 0, 'element' => '🪨', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 0, 'element' => '', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 0, 'element' => '🪙', 'color' => 'bg-sky-200'],
+                ['gravity' => 1, 'win' => 1, 'element' => '', 'color' => 'bg-sky-200'],
+            ],
+            [
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-yellow-800'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-yellow-800'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-yellow-800'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-yellow-800'],
+                ['gravity' => 0, 'win' => 0, 'element' => '🕳️', 'color' => 'bg-yellow-800'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-yellow-800'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-yellow-800'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-yellow-800'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-yellow-800'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-yellow-800'],
+                ['gravity' => 0, 'win' => 0, 'element' => '', 'color' => 'bg-yellow-800'],
+            ]
+        ];
+    }
+
+    public function moveLeft()
+    {
+        if (! $this->alive) {
+            return;
+        }
+        $this->announcements = [];
+
+        $this->board[$this->y][$this->x]['element'] = '';
+        $this->x--;
+
+        $this->checkNewPosition();
+    }
+
+    public function moveRight()
+    {
+        if (! $this->alive) {
+            return;
+        }
+        $this->announcements = [];
+
+        $this->board[$this->y][$this->x]['element'] = '';
+        $this->x++;
+
+        $this->checkNewPosition();
+    }
+
+    public function moveUp()
+    {
+        if (! $this->alive) {
+            return;
+        }
+        $this->announcements = [];
+
+        $this->board[$this->y][$this->x]['element'] = '';
+        $this->y--;
+        $this->checkNewPosition();
+    }
+
+    public function jump()
+    {
+        if (! $this->alive) {
+            return;
+        }
+        $this->announcements = [];
+
+        $this->board[$this->y][$this->x]['element'] = '';
+        $this->x++;
+        if (isset($this->board[$this->y + 1][$this->x + 1]) && $this->board[$this->y + 1][$this->x]['element'] == '🕳️') {
+            $this->x++;
+        }
+        if ($this->y > 0) {
+            $this->y--;
+            $this->checkNewPosition();
+        }
+    }
+
+    protected function checkNewPosition(): void
+    {
+        if (isset($this->board[$this->y][$this->x])) {
+            if ($this->board[$this->y][$this->x]['win'] == 1) {
+                $this->announcements[] = 'Has ganado!';
+                $this->hasWon = true;
+                $this->score += $this->time;
+                $this->board[$this->y][$this->x]['element'] = '👩‍🦯';
+                return;
+            }
+
+            switch ($this->board[$this->y][$this->x]['element']) {
+                case '🪙':
+                    $this->score++;
+                    $this->board[$this->y][$this->x]['element'] = '💃';
+                    $this->announcements[] = 'Has ganado una moneda!';
+                    break;
+                case '🪨':
+                    $this->alive = false;
+                    $this->announcements[] = 'Te comiste una roca. Estás muerto';
+                    $this->board[$this->y][$this->x]['element'] = '😱';
+                    break;
+                case '🕳️':
+                    $this->alive = false;
+                    $this->announcements[] = 'Te caíste al agujero! Estás muerto';
+                    $this->board[$this->y][$this->x]['element'] = '😱';
+                    break;
+                default:
+                    $this->board[$this->y][$this->x]['element'] = '💃';
+                    $this->ask();
+            }
+        }
+    }
+
+    public function ask()
+    {
+        $this->announcements = [];
+
+        if (! $this->alive) {
+            $this->announcements[] = 'Estás muerto!';
+            return;
+        }
+
+        if ($this->hasWon) {
+            $this->announcements[] = 'Has ganado!';
+            return;
+        }
+
+        if (isset($this->board[$this->y][$this->x + 1])) {
+            switch ($this->board[$this->y][$this->x + 1]['element']) {
+                case '🪙':
+                    $this->announcements[] = 'Moneda delante';
+                    break;
+                case '🪨':
+                    $this->announcements[] = 'Hay una roca delante';
+                    break;
+                case '🕳️':
+                    $this->announcements[] = 'Hay un agujero delante';
+                    break;
+                default:
+                    $this->announcements[] = 'La siguiente casilla está vacía';
+                    break;
+            }
+        }
+
+        if (isset($this->board[$this->y - 1][$this->x])) {
+            switch ($this->board[$this->y - 1][$this->x]['element']) {
+                case '🪙':
+                    $this->announcements[] = 'Moneda arriba';
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (isset($this->board[$this->y + 1][$this->x + 1]['element'])) {
+            switch ($this->board[$this->y + 1][$this->x + 1]['element']) {
+                case '🕳️':
+                    $this->announcements[] = 'Agujero delante';
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (isset($this->board[$this->y + 1][$this->x])) {
+            if ($this->board[$this->y + 1][$this->x]['element'] === '🪙') {
+                $this->announcements[] = 'Moneda arriba';
+            }
+        }
+    }
+
+    public function describeEmoji($element) {
+        switch($element) {
+            case '🪙':
+                return 'Moneda';
+            case '🪨':
+                return 'Roca';
+            case '🕳️':
+                return 'Agujero';
+            case '💃':
+                return 'Personaje';
+            default:
+                return 'Vacio';
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.game');
+    }
+
+}
---
+++ resources/views/livewire/game.blade.php
@@ -0,0 +1,89 @@
+<div>
+    @if($showInfo)
+        <div class="grid h-screen place-items-center">
+            <div class="text-3xl text-center max-w-4xl margin-auto">
+                <p>Hola!
+                    Este es un juego en el que tienes que llegar al final a tiempo y recolectar las monedas que encuentres.
+                    Para no morir deber saltar las rocas y evitar los agujeros.
+                </p>
+
+                <p>Al darle al botón de acceder se cargará el tablero y se hará focus en el boton de jugar
+                </p>
+
+                <ul class="mt-4" role="menu">
+                    Si pulsas:
+                    <li>Shift: Preguntas que hay alrededor</li>
+                    <li>Flecha derecha: avanzas hacia la derecha</li>
+                    <li>Flecha izquierda: avanzas hacia la izquierda</li>
+                    <li>Espacio: Saltas</li>
+                    <li>Flecha arriba: subes sin avanzar</li>
+                </ul>
+            </div>
+            <button wire:click="startGame"
+                    class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Empezar
+            </button>
+        </div>
+    @endif
+
+    @if($showBoard)
+        <div class="grid h-screen place-items-center"
+             wire:poll.1s="tick">
+            <div class="flex justify-end">
+                <div class=" w-48 text-7xl text-center align-content-center"
+                     aria-label="puntuación">
+                    {{ $score }}
+                </div>
+                <div class=" w-48 text-7xl text-center align-content-center"
+                     aria-label="tiempo">
+                    {{ $time }}''
+                </div>
+            </div>
+
+            <div class="flex border border-indigo-400 p-4 m-4" aria-live="assertive">
+                <ul>
+
+               @foreach($announcements as $announcement)
+                    <li> {{ $announcement }}</li>
+                @endforeach
+                </ul>
+            </div>
+
+            <div class="m-4">
+                <button wire:model="movement" autofocus
+                        wire:keydown.right="moveRight"
+                        wire:keydown.left="moveLeft"
+                        wire:keydown.up="moveUp"
+                        wire:keydown.space="jump"
+                        wire:keydown.shift="ask"
+                        class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        aria-label="play">
+                    Play
+                </button>
+            </div>
+
+            <table class="border-collapse border-0">
+                <thead></thead>
+                <tbody>
+                @foreach($board as $row)
+                    <tr>
+                        @foreach($row as $cell)
+                            <td>
+                                <div class="content-around grid text-center w-32 h-32 border border-gray-400 text-7xl {{ $cell['color'] ?? 'bg-gray-200' }}">
+                                    <span aria-hidden="true">{{ $cell['element'] }}</span>
+                                    <span class="sr-only">
+                                        {{ $cell['element'] === '💃' ? 'Personaje' : '' }}
+                                        {{ $cell['element'] === '🪨' ? 'Roca' : '' }}
+                                        {{ $cell['element'] === '🕳' ? 'Agujero' : '' }}
+                                        {{ $cell['element'] === '🪙' ? 'Moneda' : '' }}
+                                    </span>
+                                </div>
+                            </td>
+                        @endforeach
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    @endif
+</div>
